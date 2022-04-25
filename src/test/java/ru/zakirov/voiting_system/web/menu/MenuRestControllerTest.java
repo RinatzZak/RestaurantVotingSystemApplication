705e37bc46d33f68fdd36499ec348ea51e54c21d
Dzenovich <zakirov.zrz@gmail.com>
2022-04-25 19:53:00 +0300

    added tests for restaurant

--- src/test/java/ru/zakirov/voiting_system/web/menu/MenuRestControllerTest.java
+++ src/test/java/ru/zakirov/voiting_system/web/menu/MenuRestControllerTest.java
@@ -1,29 +1,49 @@
 package ru.zakirov.voiting_system.web.menu;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.zakirov.voiting_system.model.Menu;
+import ru.zakirov.voiting_system.repository.MenuRepository;
+import ru.zakirov.voiting_system.util.JsonUtil;
 import ru.zakirov.voiting_system.web.AbstractControllerTest;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static ru.zakirov.voiting_system.web.menu.MenuTestData.MENU_MATCHER;
-import static ru.zakirov.voiting_system.web.menu.MenuTestData.menu1;
+import static ru.zakirov.voiting_system.web.meal.MealTestData.meal5;
+import static ru.zakirov.voiting_system.web.meal.MealTestData.meal8;
+import static ru.zakirov.voiting_system.web.menu.MenuTestData.*;
 import static ru.zakirov.voiting_system.web.restaurant.RestaurantTestData.RESTAURANT1_ID;
+import static ru.zakirov.voiting_system.web.user.UserTestData.ADMIN_MAIL;
 import static ru.zakirov.voiting_system.web.user.UserTestData.USER_MAIL;
 
 class MenuRestControllerTest extends AbstractControllerTest {
 
+    @Autowired
+    private MenuRepository menuRepository;
+
     @Test
-    void delete() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete("/api/admin/restaurants/1/menu/" + MENU1_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertFalse(menuRepository.findById(MENU1_ID).isPresent());
     }
 
     @Test
     @WithUserDetails(value = USER_MAIL)
     void getForRestaurant() throws Exception {
-        perform(MockMvcRequestBuilders.get("/api/profile/restaurants/" + RESTAURANT1_ID + "/menu"))
+        perform(MockMvcRequestBuilders.get("/api/restaurants/" + RESTAURANT1_ID + "/menu"))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -31,22 +51,73 @@ class MenuRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void getAll() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get("/api/admin/menus"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MENU_MATCHER.contentJson(menuList));
     }
 
     @Test
-    void createWithLocation() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        Menu newMenu = MenuTestData.getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post("/api/admin/restaurants/4/menu")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newMenu)))
+                .andExpect(status().isCreated());
+
+        Menu created = MENU_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newMenu.setId(newId);
+        MENU_MATCHER.assertMatch(created, newMenu);
+        MENU_MATCHER.assertMatch(menuRepository.getById(newId), newMenu);
     }
 
     @Test
-    void update() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void createDoubletMenu() throws Exception {
+        Menu newMenu = MenuTestData.getNew();
+        perform(MockMvcRequestBuilders.post("/api/admin/restaurants/1/menu")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newMenu)))
+                .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
-    void addingMealToMenu() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void update() throws Exception {
+        try {
+            MockedStatic<LocalDate> mocked = Mockito.mockStatic(LocalDate.class);
+            mocked.when(LocalDate::now).thenReturn(LocalDate.of(2030, 3, 11));
+            Menu updated = MenuTestData.getUpdated();
+            perform(MockMvcRequestBuilders.put("/api/admin/restaurants/1/menu/" + MENU1_ID)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(JsonUtil.writeValue(updated)))
+                    .andExpect(status().isNoContent());
+
+            MENU_MATCHER.assertMatch(menuRepository.getById(MENU1_ID), updated);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void removingMealFromMenu() {
+    @WithUserDetails(value = ADMIN_MAIL)
+    void addingMealToMenu() throws Exception {
+        perform(MockMvcRequestBuilders.post("/api/admin/restaurants/1/menu/meals/" + meal8.id())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(menu1)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void removingMealFromMenu() throws Exception {
+        perform(MockMvcRequestBuilders.delete("/api/admin/restaurants/1/menu/meals/" + meal5.id())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(menu1)))
+                .andExpect(status().isNoContent());
     }
 }
--- src/test/java/ru/zakirov/voiting_system/web/menu/MenuTestData.java
+++ src/test/java/ru/zakirov/voiting_system/web/menu/MenuTestData.java
@@ -1,10 +1,11 @@
 package ru.zakirov.voiting_system.web.menu;
 
-import org.assertj.core.util.DateUtil;
 import ru.zakirov.voiting_system.model.Meal;
 import ru.zakirov.voiting_system.model.Menu;
 import ru.zakirov.voiting_system.web.MatcherFactory;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static ru.zakirov.voiting_system.web.meal.MealTestData.*;
@@ -15,14 +16,22 @@ public class MenuTestData {
 
     public static final int MENU1_ID = 1;
 
-    public static Menu menu1 = new Menu(MENU1_ID, DateUtil.now(), restaurant1, List.of(meal1, meal2, meal5));
-    public static Menu menu2 = new Menu(MENU1_ID + 1, DateUtil.now(), restaurant3, List.of(meal6, meal8, meal9));
-    public static Menu menu3 = new Menu(MENU1_ID + 2, DateUtil.now(), restaurant2, List.of(meal1, meal3, meal6));
+    public static Menu menu1 = new Menu(MENU1_ID, LocalDate.now(), restaurant1, List.of(meal1, meal2, meal5));
+    public static Menu menu2 = new Menu(MENU1_ID + 1, LocalDate.now(), restaurant3, List.of(meal6, meal8, meal9));
+    public static Menu menu3 = new Menu(MENU1_ID + 2, LocalDate.now(), restaurant2, List.of(meal1, meal3, meal6));
 
+    public static List<Menu> menuList = List.of(menu1, menu2, menu3);
 
 
     public static final List<Meal> meals1 = List.of(meal1, meal2, meal5);
     public static final List<Meal> meals2 = List.of(meal6, meal8, meal9);
     public static final List<Meal> meals3 = List.of(meal1, meal3, meal6);
 
+    public static Menu getNew() {
+        return new Menu(null, LocalDate.now(), restaurant4, new ArrayList<>());
+    }
+
+    public static Menu getUpdated() {
+        return new Menu(MENU1_ID, LocalDate.of(2030, 3, 11), restaurant1, List.of(meal1, meal2, meal5));
+    }
 }

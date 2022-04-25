169f72f23ae9e3f4483d4c975f1aa95a8f6cf096
Dzenovich <zakirov.zrz@gmail.com>
2022-04-25 19:55:12 +0300

    added new restaurant

--- src/test/java/ru/zakirov/voiting_system/web/restaurant/RestaurantTestData.java
+++ src/test/java/ru/zakirov/voiting_system/web/restaurant/RestaurantTestData.java
@@ -14,8 +14,9 @@ public class RestaurantTestData {
     public static final Restaurant restaurant1 = new Restaurant(RESTAURANT1_ID, "Kazuya", "193 Synods Street Eden Terrace");
     public static final Restaurant restaurant2 = new Restaurant(RESTAURANT1_ID + 1, "Ahi", "Commercial Bay Level 2/7 Queen Street");
     public static final Restaurant restaurant3 = new Restaurant(RESTAURANT1_ID + 2, "Culprit", "Level 1/12 Wyndham Street");
+    public static final Restaurant restaurant4 = new Restaurant(RESTAURANT1_ID + 3, "Hello Beasty", "95-97 Customs Street West Market Square");
 
-    public static final List<Restaurant> restaurants = List.of(restaurant1, restaurant2, restaurant3);
+    public static final List<Restaurant> restaurants = List.of(restaurant1, restaurant2, restaurant3, restaurant4);
 
     public static Restaurant getNew() {
         return new Restaurant(null, "NewRestaurant", "NewAddress 777");

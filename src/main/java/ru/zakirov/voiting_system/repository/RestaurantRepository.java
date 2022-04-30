ec301ca6854f424be176a7dbb0f565c93d7a8dde
Dzenovich <zakirov.zrz@gmail.com>
2022-04-30 17:58:08 +0300

    added method get with IgnoreCase for valid

--- src/main/java/ru/zakirov/voiting_system/repository/RestaurantRepository.java
+++ src/main/java/ru/zakirov/voiting_system/repository/RestaurantRepository.java
@@ -3,6 +3,9 @@ package ru.zakirov.voiting_system.repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.zakirov.voiting_system.model.Restaurant;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
+    Optional<Restaurant> getByDescriptionIgnoreCase(String description);
 }

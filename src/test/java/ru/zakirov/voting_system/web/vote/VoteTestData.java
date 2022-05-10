1bb63817c894c7a5e9e9d4286c10786b4bc7b985
Dzenovich <zakirov.zrz@gmail.com>
2022-05-10 21:10:48 +0300

    fixed votes changed localdate

--- src/test/java/ru/zakirov/voting_system/web/vote/VoteTestData.java
+++ src/test/java/ru/zakirov/voting_system/web/vote/VoteTestData.java
@@ -3,6 +3,7 @@ package ru.zakirov.voting_system.web.vote;
 import ru.zakirov.voting_system.model.Vote;
 import ru.zakirov.voting_system.web.MatcherFactory;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -12,20 +13,23 @@ import static ru.zakirov.voting_system.web.user.UserTestData.admin;
 import static ru.zakirov.voting_system.web.user.UserTestData.user;
 
 public class VoteTestData {
-    public static final MatcherFactory.Matcher<Vote> VOTE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Vote.class, "user");
+    public static final MatcherFactory.Matcher<Vote> VOTE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Vote.class, "user.password", "user.registered");
 
     public static final int VOTE1_ID = 1;
 
-    public static Vote vote1 = new Vote(VOTE1_ID, LocalTime.now().truncatedTo(ChronoUnit.MINUTES),
-            restaurant1, user);
+    public static Vote vote1 = new Vote(VOTE1_ID, LocalDate.of(2020, 10, 10), restaurant1, user);
+    public static Vote vote2 = new Vote(VOTE1_ID + 1, LocalDate.of(2021, 11, 11), restaurant2, user);
+    public static Vote vote3 = new Vote(VOTE1_ID + 2, LocalDate.of(2022, 1, 1), restaurant3, user);
 
-    public static List<Vote> voteList = List.of(vote1);
+    public static Vote vote4 = new Vote(VOTE1_ID + 3, LocalDate.now(), restaurant1, user);
+
+    public static List<Vote> myVotes = List.of(vote1, vote2, vote3);
 
     public static Vote getNew() {
-        return new Vote(null, LocalTime.now().truncatedTo(ChronoUnit.MINUTES), restaurant2, admin);
+        return new Vote(null, LocalDate.now(), restaurant2, user);
     }
 
     public static Vote getUpdated() {
-        return new Vote(VOTE1_ID, LocalTime.now().truncatedTo(ChronoUnit.MINUTES), restaurant3, user);
+        return new Vote(VOTE1_ID, LocalDate.now(), restaurant3, user);
     }
 }

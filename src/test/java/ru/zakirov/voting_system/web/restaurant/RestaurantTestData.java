d8479fb465f1ba4813ca8ce50cea906720fad100
Dzenovich <zakirov.zrz@gmail.com>
2022-05-13 22:21:36 +0300

    added matcher with dishes for tests

--- src/test/java/ru/zakirov/voting_system/web/restaurant/RestaurantTestData.java
+++ src/test/java/ru/zakirov/voting_system/web/restaurant/RestaurantTestData.java
@@ -4,9 +4,18 @@ import ru.zakirov.voting_system.model.Restaurant;
 import ru.zakirov.voting_system.web.MatcherFactory;
 
 import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.zakirov.voting_system.web.dish.DishTestData.*;
 
 public class RestaurantTestData {
     public static final MatcherFactory.Matcher<Restaurant> RESTAURANT_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Restaurant.class);
+    public static final MatcherFactory.Matcher<Restaurant>RESTAURANT_WITH_DISH_MATCHER = MatcherFactory.usingAssertions(Restaurant.class,
+            (a, e) -> assertThat(a).usingRecursiveComparison().ignoringFields("dishes.restaurant").isEqualTo(e),
+            (a, e) -> {
+                throw new UnsupportedOperationException();
+            });
 
     public static final int RESTAURANT1_ID = 1;
     public static final int NOT_FOUND_ID = 100;
@@ -17,6 +26,10 @@ public class RestaurantTestData {
     public static final Restaurant restaurant4 = new Restaurant(RESTAURANT1_ID + 3, "Hello Beasty", "95-97 Customs Street West Market Square");
 
     public static final List<Restaurant> restaurants = List.of(restaurant1, restaurant2, restaurant3, restaurant4);
+
+    static {
+        restaurant1.setDishes(List.of(dish7));
+    }
 
     public static Restaurant getNew() {
         return new Restaurant(null, "NewRestaurant", "NewAddress 777");

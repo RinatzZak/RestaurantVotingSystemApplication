4368b766c35588134b2a69cf726b76bc7dc0f143
Dzenovich <zakirov.zrz@gmail.com>
2022-05-13 19:50:18 +0300

    added dish list

--- src/main/java/ru/zakirov/voting_system/model/Restaurant.java
+++ src/main/java/ru/zakirov/voting_system/model/Restaurant.java
@@ -1,15 +1,19 @@
 package ru.zakirov.voting_system.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "restaurant")
@@ -23,16 +27,23 @@ public class Restaurant extends NamedEntity {
     @Size(min = 5, max = 100)
     private String address;
 
-    public Restaurant(Restaurant r) {
-        this(r.id, r.name, r.address);
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "restaurant_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonManagedReference
+    @ToString.Exclude
+    private List<Dish> dishes;
+
+    public Restaurant(Integer id, String name, String address) {
+        super(id, name);
+        this.address = address;
     }
 
     public Restaurant(String name, String address) {
         this(null, name, address);
     }
 
-    public Restaurant(Integer id, String name, String address) {
-        super(id, name);
-        this.address = address;
+    public Restaurant(Restaurant r) {
+        this(r.id, r.name, r.address);
     }
 }

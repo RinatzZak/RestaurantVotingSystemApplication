0fe30f2d3bd4b869de72af3fdbc6d45c618df8ba
Dzenovich <zakirov.zrz@gmail.com>
2022-05-14 16:09:43 +0300

    added @JsonIgnoreProperties(value = {"address","dishes"}) for restaurant

--- src/main/java/ru/zakirov/voting_system/model/Vote.java
+++ src/main/java/ru/zakirov/voting_system/model/Vote.java
@@ -1,6 +1,7 @@
 package ru.zakirov.voting_system.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +31,7 @@ public class Vote extends BaseEntity {
     @JoinColumn(name = "restaurant_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @ToString.Exclude
+    @JsonIgnoreProperties(value = {"address","dishes"})
     private Restaurant restaurant;
 
     @ManyToOne(fetch = FetchType.LAZY)
